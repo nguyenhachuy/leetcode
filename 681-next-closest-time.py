75ace1df3299aa1d499e0a6f331e579bea5a34ad
Gus Nguyen <nguyenhachuy@gmail.com>
2020-12-08 23:48:41 -0800

    Update 681-next-closest-time.py

--- 681-next-closest-time.py
+++ 681-next-closest-time.py
@@ -1,38 +1,31 @@
 import bisect
-
 class Solution:
     def nextClosestTime(self, time: str) -> str:
-        digits = []
-        for c in time:
-            if c.isdigit():
-                digits.append(c)
-
-        le_6 = [x for x in digits if x < 6]
-        le_2 = [x for x in digits if x <= 2]
+        digits = [x for x in time if x != ":"]
+        combinations = [x+y for x in digits for y in digits]
 
         hour, minutes = time.split(":")
-        next_closest_single_minute = bisect.bisect_left(digits, minutes[-1] + 1)
-        if next_closest_single_minute < len(digits):
-            return time[:-1] + digits[next_closest_single_minute]
+        time_to_minutes = int(hour)*60 + int(minutes)
+        min_diff = math.inf
+        result = ""
+        min_time = ""
+        min_possible_time = math.inf
+        for combination_a in combinations:
+            for combination_b in combinations:
+                if int(combination_a) < 24 and int(combination_b) < 60:
+                    new_time_minutes = int(combination_a)*60 + int(combination_b)
+                    if new_time_minutes < min_possible_time:
+                        min_possible_time = new_time_minutes
+                        min_time = combination_a + ":" + combination_b
+                    if new_time_minutes > time_to_minutes and new_time_minutes - time_to_minutes < min_diff:
+                        min_diff = abs(new_time_minutes - time_to_minutes)
+                        result = combination_a + ":" + combination_b
 
-        next_closest_double_minute = len(le_2)
-        if len(minutes) == 2:
-            next_closest_double_minute = bisect.bisect_left(le_2, minutes[-2] + 1)
-            if next_closest_double_minute < len(le_2) and next_closest_single_minute < len(digits):
-                return hour + ":" + le_2[next_closest_double_minute] + digits[next_closest_single_minute]
-
-        next_closest_single_hour = bisect.bisect_left(digits, hour[-1] + 1)
-        if next_closest_single_hour < len(digits):
-            
-def main():
-  sol = Solutions()
-  print(sol.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3]]))
-
-if __name__ == "__main__":
-  main()
-
+        if not result:
+            return min_time
+        return result
 
 """
 hour = 0,1,2
-hour2 = 
+hour2 =
 """
